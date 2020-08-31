1cd88c01f1b84f07cfe458137f358d7884c4ebf4
tfalque.ext <thibault.falque@exakis-nelite.com>
2020-08-31 11:54:50 +0200

    :sparkles: :bug:  Fixes the session problem #20

--- metrics/studio/web/application.py
+++ metrics/studio/web/application.py
@@ -1,34 +1,36 @@
-import json
-import os
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
 import base64
 import datetime
 import io
+import os
+import uuid
+
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
 import dash_table
+import jsonpickle.ext.pandas as jsonpickle_pd
 import pandas as pd
+from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-from flask_session import Session
-from flask import session
+from flask_caching import Cache
 
 from metrics.scalpel.parser import CsvCampaignParser
+from metrics.studio.web.analysis import AnalysisWeb
+from metrics.studio.web.config import external_stylesheets, PLOTLY_LOGO
 from metrics.studio.web.layout import data_loading, plots, table
 from metrics.studio.web.util import create_listener, decode
-from metrics.wallet.dataframe.builder import CampaignDataFrameBuilder
 from metrics.wallet.figure.dynamic_figure import BoxPlotly, CactusPlotly
 
-PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+jsonpickle_pd.register_handlers()
 
-external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.server.secret_key = os.urandom(24)
-server = app.server
-SESSION_TYPE = 'memcached'
-Session(app)
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': 'cache-directory',
+    'CACHE_THRESHOLD': 200
+})
 
 MAIN_MENU_TABS = {'Data loading': data_loading, 'Plots': plots, 'Table': table}
 
@@ -51,13 +53,34 @@ navbar = dbc.Navbar(
     ]
 )
 
-app.layout = html.Div(className='container', children=[
-    navbar,
-    dcc.Tabs(id="main-menu", value='Data loading', className="mt-5", children=[
-        dcc.Tab(label=k, value=k, children=v()) for k, v in MAIN_MENU_TABS.items()
 
-    ]),
-])
+def serve_layout():
+    session_id = str(uuid.uuid4())
+    return html.Div(className='container', children=[
+        html.Div(session_id, id='session-id', style={'display': 'none'}),
+        navbar,
+        dcc.Tabs(id="main-menu", value='Data loading', className="mt-5", children=[
+            dcc.Tab(label=k, value=k, children=v()) for k, v in MAIN_MENU_TABS.items()
+
+        ]),
+    ])
+
+
+app.layout = serve_layout
+
+
+def get_campaign(session_id, contents, input, sep, time, xp_ware):
+    @cache.memoize()
+    def query_and_serialize_data(session_id, contents, input, sep, time, xp_ware):
+        listener = create_listener(xp_ware, input, time)
+        csv_parser = CsvCampaignParser(listener, separator=sep)
+        csv_parser.parse_stream(decode(contents))
+        campaign = listener.get_campaign()
+        analysis_web = AnalysisWeb(campaign)
+        campaign_df = analysis_web.campaign_df
+        return campaign_df, campaign
+
+    return query_and_serialize_data(session_id, contents, input, sep, time, xp_ware)
 
 
 def parse_contents(contents, filename, date, separator=','):
@@ -106,25 +129,13 @@ def update_output(list_of_contents, list_of_names, list_of_dates, sep):
 
 
 @app.callback([Output('box', 'children'), Output('cactus', 'children')],
-              [Input('xp-ware', 'value'), Input('time', 'value'),
+              [Input('session-id', 'children'), Input('xp-ware', 'value'), Input('time', 'value'),
                Input('input', 'value')],
               [State('upload-data', 'contents'), State('sep', 'value')])
-def campaign_callback(xp_ware, time, input, contents, sep):
+def campaign_callback(session_id, xp_ware, time, input, contents, sep):
     if contents is None or input is None or time is None or xp_ware is None:
         raise PreventUpdate
-    campaign_df, campaign = create_campaign_df(contents, input, sep, time, xp_ware) if session.get('campaign',
-                                                                                                   None) is None \
-        else session.get('campaign')
-    session['campaign'] = json.dumps(campaign)
+    campaign_df, campaign = get_campaign(session_id, contents, input, sep, time, xp_ware)
     box = BoxPlotly(campaign_df)
     cactus = CactusPlotly(campaign_df, show_marker=False)
     return [dcc.Graph(figure=box.get_figure()), ], [dcc.Graph(figure=cactus.get_figure()), ]
-
-
-def create_campaign_df(contents, input, sep, time, xp_ware):
-    listener = create_listener(xp_ware, input, time)
-    csv_parser = CsvCampaignParser(listener, separator=sep)
-    csv_parser.parse_stream(decode(contents))
-    campaign = listener.get_campaign()
-    campaign_df = CampaignDataFrameBuilder(campaign).build_from_campaign()
-    return campaign_df, campaign
---
+++ metrics/studio/web/config.py
@@ -0,0 +1,3 @@
+import dash_bootstrap_components as dbc
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
+external_stylesheets = [dbc.themes.BOOTSTRAP]
