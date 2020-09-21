63d72fac79d454d1f449700c9456eff74825f2e2
tfalque.ext <thibault.falque@exakis-nelite.com>
2020-09-21 14:30:27 +0200

    :construction_worker: CI.

--- run.sh
+++ run.sh
@@ -1,2 +1,23 @@
-cd /usr/src/app/
-gunicorn metrics.studio.web.application:server -b 0.0.0.0:8000
+#!/bin/sh
+
+NAME="metrics"                                  # Name of the application
+APPDIR=/home/metrics/metrics-master             # App project directory
+app="metrics.studio.web.application:server"
+NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn
+LOGDIRS="$APPDIR/logs"
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $APPDIR || exit
+export PYTHONPATH=$APPDIR:$PYTHONPATH
+
+# Create the log directory if it doesn't exist
+test -d $LOGDIRS || mkdir -p $LOGDIRS
+
+touch $LOGDIRS/gunicorn.log
+
+echo "gunicorn $APP --name $NAME --workers $NUM_WORKERS --log-level=debug --log-file=$LOGDIRS/logs/gunicorn.log"
+# Start your app with gunicorn
+# $APPDIR/venv/bin/gunicorn $app --name $NAME --workers $NUM_WORKERS --bind=127.0.0.1:8000 --log-level=debug --log-file=$LOGDIRS/gunicorn.log --daemon
+$APPDIR/venv/bin/gunicorn $app --name $NAME --workers $NUM_WORKERS --bind=127.0.0.1:8000 --log-level=debug --log-file=$LOGDIRS/gunicorn.log
