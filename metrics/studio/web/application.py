3b2d4b4e5588eabf8a6014e388625ea6be1dae60
tfalque.ext <thibault.falque@exakis-nelite.com>
2020-11-16 13:17:02 +0100

    Add about modal

--- metrics/studio/web/application.py
+++ metrics/studio/web/application.py
@@ -224,6 +224,16 @@ def display_page(pathname):
         return serve_layout_with_content(campaign)
     return serve_normal_layout()
 
+@dash.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
 
 @dash.callback([Output('xp-ware', 'options'),
                 Output('time', 'options'),
