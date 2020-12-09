eb5df3ddb57bc37f07db5d4e01fc3c8a81b392e3
tfalque.ext <thibault.falque@exakis-nelite.com>
2020-12-09 13:09:19 +0100

    :white_check_mark: Adds unit test.

---
+++ tests/test_scalpel/test_output.py
@@ -0,0 +1,16 @@
+import os
+from unittest import TestCase
+
+from metrics.wallet import Analysis
+
+
+class TestReadConfigurationBug(TestCase):
+    def setUp(self) -> None:
+        self._example_dir = 'example/'
+
+    def test_bug_38(self):
+        analysis = Analysis(
+            input_file=os.path.join(self._example_dir, 'issue38/campaign/config/metrics_scalpel.yml'))
+        columns = analysis.campaign_df.data_frame.columns
+        self.assertTrue('best_bound' in columns)
+        self.assertTrue('status' in columns)
