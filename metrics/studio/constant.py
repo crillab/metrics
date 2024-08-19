96ccb74803d136ecf835d0eda5ba9137d4cfa0c9
Thibault Falque <thibault.falque@uni.lu>
2024-08-19 19:12:29 +0200

    some constants

---
+++ metrics/studio/constant.py
@@ -0,0 +1,8 @@
+import os
+
+METRICS_DIR_EXPERIMENT_WARE = "experiment_wares"
+METRICS_DIR_CONFIG = "config"
+METRICS_DIR_EXPERIMENTS = "experiments"
+METRICS_DIR_INPUT_SET = "input_set"
+
+METRICS_TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), "../templates")
