0e9a1eabe1489084dd3cf0cccb3a2b553882e57a
Thibault Falque <thibault.falque@uni.lu>
2024-12-04 10:15:23 +0100

    removes useless code

--- metrics/studio/campaign.py
+++ metrics/studio/campaign.py
@@ -29,167 +29,35 @@ class CampaignModel(Model):
         database = db
 
 
-# class Campaign:
-#     # campaign_directory, host, port, cpu, wall, memout, delay, qos, parition, nNodes, nCpu,
-#     def __init__(self, root_dir='.'):
-#         self._env = Environment(loader=PackageLoader('metrics'), autoescape=select_autoescape())
-#         self._root_dir = root_dir
-#         self._template_vars = {'slurm': {}, 'runsolver': {}, 'ssh': {}}
-#
-#     @property
-#     def campaign_dir(self):
-#         return self._template_vars['ssh'].get('campaign_directory')
-#
-#     @property
-#     def ssh_hostname(self):
-#         return self._template_vars['ssh']['host']
-#
-#     def _write_template(self, template_name: str, output_file: str) -> None:
-#         """
-#         Writes a report file following a template.
-#
-#         :param template_name: The name of the file to use as template.
-#         :param output_file: The path of the output file (relative to the root directory of
-#                             the report).
-#         """
-#         with open(os.path.join(self._root_dir, output_file), 'w') as file:
-#             template = self._env.get_template(template_name)
-#             all_vars = self._template_vars["ssh"]
-#             all_vars.update(self._template_vars["slurm"])
-#             all_vars.update(self._template_vars["runsolver"])
-#             print(template.render(**all_vars), file=file)
-#
-#     def __setitem__(self, key: str, value: Any) -> None:
-#         """
-#         Sets the value of an item that should be rendered in a template.
-#
-#         :param key: The name of the value to set.
-#         :param value: The value to set.
-#         """
-#         self._template_vars[key] = value
-#
-#     def __getitem__(self, item: str) -> Any:
-#         """
-#         Gives the value of an item that should be rendered in a template.
-#
-#         :param item: The name of the value to get.
-#
-#         :return: The value associated with the given name.
-#         """
-#         return self._template_vars[item]
-#
-#     def update_dict(self, yaml_data):
-#         self._template_vars.update(yaml_data)
-#
-#     def save(self):
-#         self._write_template('campaign.yml', 'campaign.yml')
-#
-#
-# def campaigns_init_from_argument(arguments, cb: 'CampaignConfigBuilder'):
-#     if "campaign_directory" in arguments and arguments["campaign_directory"]:
-#         cb.add_campaign_directory(arguments["campaign_directory"])
-#     if "host" in arguments and arguments["host"]:
-#         cb.add_host(arguments["host"])
-#     if "port" in arguments and arguments["port"]:
-#         cb.add_port(arguments["port"])
-#     if "qos" in arguments and arguments["qos"]:
-#         cb.add_qos(arguments["qos"])
-#     if "partition" in arguments and arguments["partition"]:
-#         cb.add_partition(arguments["partition"])
-#     if "n_nodes" in arguments and arguments["n_nodes"]:
-#         cb.add_n_nodes(arguments["n_nodes"])
-#     if "n_cpu" in arguments and arguments["n_cpu"]:
-#         cb.add_partition(arguments["n_cpu"])
-#     if "cpu" in arguments and arguments["cpu"]:
-#         cb.add_cpu_timeout(arguments["cpu"])
-#     if "wall" in arguments and arguments["wall"]:
-#         cb.add_wall_timeout(arguments["wall"])
-#     if "delay" in arguments and arguments["delay"]:
-#         cb.add_delay(arguments["delay"])
-#     if "memout" in arguments and arguments["memout"]:
-#         cb.add_memout(arguments["memout"])
-#     return cb
-#
-#
-# def campaigns_init_from_dict(yaml_data, cb: 'CampaignConfigBuilder'):
-#     print(yaml_data)
-#     cb.add_data_from_dict(yaml_data)
-#     return cb
-#
-#
-# class CampaignFactory:
-#     @staticmethod
-#     def campaign_builder(arguments, file) -> 'CampaignConfigBuilder':
-#         cb = CampaignConfigBuilder()
-#         if os.path.exists(file):
-#             with open(file, "r") as stream:
-#                 try:
-#                     data = yaml.safe_load(stream)
-#                     loguru.logger.debug(data)
-#                     campaigns_init_from_dict(data, cb)
-#                 except yaml.YAMLError as exc:
-#                     loguru.logger.error(f"Failed to parse the 'campaign.yml' file. {exc}")
-#         loguru.logger.debug(arguments)
-#         cb = campaigns_init_from_argument(arguments, cb)
-#         return cb
+class Campaign:
+    # campaign_directory, host, port, cpu, wall, memout, delay, qos, parition, nNodes, nCpu,
+    def __init__(self, root_dir='.', config: Dict[str, Any] = None):
+        self._root_dir = root_dir
+        self._config = config
 
+    @property
+    def campaign_dir(self):
+        return self._config['ssh'].get('campaign_directory')
 
-# class CampaignConfigBuilder(AbstractCampaignBuilder):
-#     def __init__(self, root_dir=os.getcwd()):
-#         super().__init__(root_dir)
-#
-#
-#
-#     def update_vars(self, dict: Dict[str, Any]) -> None:
-#         """
-#         Updates the values of the internal directory with another directory.
-#
-#         :param dict: The items to update and their new values.
-#         """
-#         self._template_vars.update(dict)
-#
-#
-#
-#
-#     # def add_host(self, host):
-#     #     self._campaign["ssh"]["host"] = host
-#     #
-#     # def add_port(self, port):
-#     #     self._campaign["ssh"]["port"] = port
-#     #
-#     # def add_campaign_directory(self, cp):
-#     #     self._campaign["ssh"]["campaign_directory"] = cp
-#     #
-#     # def add_cpu_timeout(self, timeout):
-#     #     self._campaign["runsolver"]["cpu_timeout"] = convert_to_seconds(timeout)
-#     #
-#     # def add_wall_timeout(self, timeout):
-#     #     self._campaign["slurm"]["wall_timeout"] = convert_to_seconds(timeout)
-#     #
-#     # def add_memout(self, memout):
-#     #     self._campaign["runsolver"]["memory_out"] = memout
-#     #
-#     # def add_delay(self, delay):
-#     #     self._campaign["runsolver"]["delay"] = convert_to_seconds(delay)
-#     #
-#     # def add_qos(self, qos):
-#     #     self._campaign["slurm"]["qos"] = qos
-#     #
-#     # def add_partition(self, partition):
-#     #     self._campaign["slurm"]["partition"] = partition
-#     #
-#     # def add_n_nodes(self, nNodes):
-#     #     self._campaign["slurm"]["n_nodes"] = nNodes
-#     #
-#     # def add_n_cpu(self, nCpu):
-#     #     self._campaign["slurm"]["n_cpu"] = nCpu
-#     #
-#     # def add_data_from_dict(self, d):
-#     #     self._campaign.update_dict(d)
-#     #
-#     # def build(self) -> Campaign:
-#     #     return self._campaign
+    @property
+    def ssh_hostname(self):
+        return self._config['ssh']['host']
 
+    @property
+    def cpu_time(self):
+        return self._config['setup']['timeout']
+
+    @property
+    def wall_time(self):
+        return self._config['setup']['wall_timeout']
+
+    @property
+    def memout(self):
+        return self._config['setup']['memout']
+
+    @property
+    def delay(self):
+        return self._config['setup']['delay']
 
 class CampaignBuilder(TemplateBuilder):
     """
@@ -212,6 +80,7 @@ class CampaignBuilder(TemplateBuilder):
             sys.exit(1)
         with open(campaign_file, 'r', encoding='utf-8') as yaml_stream:
             self._template_vars = yaml.load(yaml_stream)
+            return Campaign(os.getcwd(), self._template_vars)
 
     def create_directories(self) -> None:
         """
@@ -297,10 +166,11 @@ class CampaignBuilder(TemplateBuilder):
         self._write_template('start.sh', os.path.join(METRICS_DIR_EXPERIMENT_WARE, 'start.sh'))
         source_scripts = os.path.join(METRICS_TEMPLATE_PATH, 'scripts')
         try:
-            shutil.copytree(source_scripts, os.path.join(self.root_dir, METRICS_DIR_EXPERIMENT_WARE),
+            shutil.copytree(source_scripts, os.path.join(self._root_dir, METRICS_DIR_EXPERIMENT_WARE),
                             dirs_exist_ok=True)
         except Exception as e:
-            loguru.logger.error(f"Error copying the directory {source_scripts} to {METRICS_DIR_EXPERIMENT_WARE}", e)
+            loguru.logger.error(f"Error copying the directory {source_scripts} to {METRICS_DIR_EXPERIMENT_WARE}")
+            loguru.logger.error(e)
 
     def add_run_solver(self) -> None:
         bin_dir = os.path.join(self._root_dir, METRICS_DIR_EXPERIMENT_WARE, "bin")
@@ -319,7 +189,7 @@ class CampaignBuilder(TemplateBuilder):
 
         try:
             m = CampaignModel.create(name=self._template_vars['title'] or self._root_dir,
-                                     local_directory=self._root_dir,
+                                     local_directory=os.path.abspath(self._root_dir),
                                      remote_directory=None)
             m.save()
         except peewee.PeeweeException as e:
