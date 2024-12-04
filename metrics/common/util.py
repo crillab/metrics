4e2ed8aaa5140a2e108837cf795390f4fae92af7
Thibault Falque <thibault.falque@uni.lu>
2024-12-04 10:15:43 +0100

    docstring

--- metrics/common/util.py
+++ metrics/common/util.py
@@ -10,18 +10,78 @@ from platformdirs import user_data_dir
 import metrics
 from metrics.studio.constant import METRICS_DIR_CONFIG
 
-UNITS = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
+UNITS_TIME = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
 
 
 def convert_to_seconds(text):
+    """
+        Convert a string representing a duration (e.g., "1h", "30m", "2.5d") into an integer representing the total time in seconds.
+
+        Args:
+            text (str): The input string containing numerical values followed by optional time units.
+                        Supported units are:
+                        - 's' for seconds
+                        - 'm' for minutes
+                        - 'h' for hours
+                        - 'd' for days
+                        - 'w' for weeks
+                        If no unit is provided, the value is assumed to be in seconds.
+
+        Returns:
+            int: The equivalent duration in seconds.
+
+        Example:
+            convert_to_seconds("1h") -> 3600
+            convert_to_seconds("30m") -> 1800
+            convert_to_seconds("2.5d") -> 216000
+            convert_to_seconds("45s") -> 45
+            convert_to_seconds("3w") -> 1814400
+            convert_to_seconds("120") -> 120
+        """
     return int(timedelta(**{
-        UNITS.get(m.group('unit').lower(), 'seconds'): float(m.group('val'))
+        UNITS_TIME.get(m.group('unit').lower(), 'seconds'): float(m.group('val'))
         for m in re.finditer(
             r'(?P<val>\d+(\.\d+)?)(?P<unit>[smhdw]?)',
             text.replace(' ', ''),
             flags=re.I
         )
     }).total_seconds())
+
+
+UNITS_SIZE = {
+    'kb': 1 / 1024,
+    'mb': 1,
+    'gb': 1024,
+    'tb': 1024 * 1024
+}
+
+
+def convert_to_megabytes(text):
+    """
+    Convert a string representing a file size (e.g., "1GB", "1024MB") into an integer representing the size in megabytes.
+
+    Args:
+        text (str): The input string containing a numerical value followed by an optional unit (KB, MB, GB, TB).
+                    If no unit is provided, the value is assumed to be in megabytes.
+
+    Returns:
+        int: The equivalent size in megabytes.
+
+    Example:
+        convert_to_megabytes("1GB") -> 1024
+        convert_to_megabytes("1024MB") -> 1024
+        convert_to_megabytes("500KB") -> 0
+        convert_to_megabytes("1TB") -> 1048576
+        convert_to_megabytes("500") -> 500
+    """
+    match = re.match(r'(?P<val>\d+(\.\d+)?)(?P<unit>[kmgbt]b?)?', text.strip().lower())
+    if not match:
+        raise ValueError("Invalid format")
+
+    value = float(match.group('val'))
+    unit = match.group('unit') or 'mb'
+
+    return int(value * UNITS_SIZE.get(unit, 1))
 
 
 def get_cache_dir() -> str:
@@ -46,6 +106,8 @@ def get_cache_ew_dir():
 def walk_through_files(path, file_extensions):
     for (dirpath, dirnames, filenames) in os.walk(path):
         for filename in filenames:
+            if len(file_extensions) == 0:
+                yield os.path.join(dirpath, filename)
             for extension in file_extensions:
                 if filename.endswith(extension):
                     yield os.path.join(dirpath, filename)
